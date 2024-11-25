6853e3d6e686be53eb72bd5a9c41d58da521836d
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-11-25 10:59:03 +0430

    new files added

--- src/main/java/com/Simple Crud/CRUD/CrudApplication.java
+++ src/main/java/com/Simple Crud/CRUD/CrudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafiArianCrudApplication {
+public class CrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafiArianCrudApplication.class, args);
+		SpringApplication.run(CrudApplication.class, args);
 	}
 
 }
